c75a2d7e8c8fab48385601be55d0a5deb1712c6e
Nicolas DUBIEN <github@dubien.org>
2018-05-17 22:36:22 +0200

    Add tape example

--- test-jest/example.spec.js
+++ test-jest/example.spec.js
@@ -1,16 +1,19 @@
 import * as fc from 'fast-check';
 
+///*bug*/ const contains = (pattern, text) => text.substr(1).indexOf(pattern) !== -1;
+const contains = (pattern, text) => text.indexOf(pattern) !== -1;
+
 test('The concatenation of a, b and c always contains b', () => {
-  ///*bug*/ const contains = (pattern, text) => text.substr(1).indexOf(pattern) !== -1;
-  const contains = (pattern, text) => text.indexOf(pattern) !== -1;
-  fc.assert(fc.property(fc.string(), fc.string(), fc.string(), (a, b, c) => contains(b, a + b + c)));
-});
-test('Also works with fc.assert on expect', () => {
-  ///*bug*/ const stringArbitrary = fc.string();
-  const stringArbitrary = fc.string(2);
   fc.assert(
-    fc.property(stringArbitrary, a => {
-      expect(a.length).toBeLessThanOrEqual(2);
+    fc.property(fc.string(), fc.string(), fc.string(), (a, b, c) => {
+      return contains(b, a + b + c);
+    })
+  );
+});
+test('Also works with expect', () => {
+  fc.assert(
+    fc.property(fc.string(), fc.string(), fc.string(), (a, b, c) => {
+      expect(contains(b, a + b + c)).toBe(true);
     })
   );
 });
--- test-tape/example.spec.js
+++ test-tape/example.spec.js
@@ -1,14 +1,16 @@
-import test from 'ava';
-import * as fc from 'fast-check';
+const test = require('tape');
+const fc = require('fast-check');
 
 ///*bug*/ const contains = (pattern, text) => text.substr(1).indexOf(pattern) !== -1;
 const contains = (pattern, text) => text.indexOf(pattern) !== -1;
 
-test('The concatenation of a, b and c always contains b', t =>
-  t.notThrows(() => {
+test('The concatenation of a, b and c always contains b', assert => {
+  assert.plan(1);
+  assert.doesNotThrow(() => {
     fc.assert(
       fc.property(fc.string(), fc.string(), fc.string(), (a, b, c) => {
         return contains(b, a + b + c);
       })
     );
-  }));
+  });
+});
