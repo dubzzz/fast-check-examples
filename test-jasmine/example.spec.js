521dbec40b5d063a5cb6dbca309ba04b323cdc2a
Nicolas DUBIEN <github@dubien.org>
2018-05-17 22:52:25 +0200

    Add jasmine example

---
+++ test-jasmine/example.spec.js
@@ -0,0 +1,23 @@
+const fc = require('fast-check');
+
+///*bug*/ const contains = (pattern, text) => text.substr(1).indexOf(pattern) !== -1;
+const contains = (pattern, text) => text.indexOf(pattern) !== -1;
+
+describe('Jasmine Example', () => {
+  it('The concatenation of a, b and c always contains b', () => {
+    fc.assert(
+      fc.property(fc.string(), fc.string(), fc.string(), (a, b, c) => {
+        return contains(b, a + b + c);
+      })
+    );
+  });
+  it('Also works with expect', () => {
+    // requires jasmine.json to set:
+    // "stopSpecOnExpectationFailure": true
+    fc.assert(
+      fc.property(fc.string(), fc.string(), fc.string(), (a, b, c) => {
+        expect(contains(b, a + b + c)).toBe(true);
+      })
+    );
+  });
+});
