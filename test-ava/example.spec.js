27f25c187e749240a93073e8abc66d19b4079852
Nicolas DUBIEN <github@dubien.org>
2018-09-06 09:05:06 +0200

    Usage of ava-fast-check in combination with chai

--- test-ava/example.spec.js
+++ test-ava/example.spec.js
@@ -1,8 +1,13 @@
 import { testProp, fc } from 'ava-fast-check';
+import { expect } from 'chai';
 
 ///*bug*/ const contains = (pattern, text) => text.substr(1).indexOf(pattern) !== -1;
 const contains = (pattern, text) => text.indexOf(pattern) !== -1;
 
 testProp('The concatenation of a, b and c always contains b', [fc.string(), fc.string(), fc.string()], (a, b, c) => {
   return contains(b, a + b + c);
+});
+
+testProp('The concatenation of a, b and c always contains b (with expect)', [fc.string(), fc.string(), fc.string()], (a, b, c) => {
+  expect(contains(b, a + b + c)).to.be.true;
 });
