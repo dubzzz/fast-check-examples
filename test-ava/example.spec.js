16f9892b66bb26f186c455bc8f120a394b958c3c
Lazerbeak12345 <nfblaster@live.com>
2022-05-06 19:56:35 -0600

    Use newer api versions
    The `ava-fast-check` library now provides the `t` context argument to each of the tests as the first argument.

--- test-ava/example.spec.js
+++ test-ava/example.spec.js
@@ -4,14 +4,14 @@ import { expect } from 'chai';
 ///*bug*/ const contains = (pattern, text) => text.substr(1).indexOf(pattern) !== -1;
 const contains = (pattern, text) => text.indexOf(pattern) !== -1;
 
-testProp('The concatenation of a, b and c always contains b', [fc.string(), fc.string(), fc.string()], (a, b, c) => {
-  return contains(b, a + b + c);
+testProp('The concatenation of a, b and c always contains b', [fc.string(), fc.string(), fc.string()], (t, a, b, c) => {
+  t.true(contains(b, a + b + c));
 });
 
 testProp(
   'The concatenation of a, b and c always contains b (with expect)',
   [fc.string(), fc.string(), fc.string()],
-  (a, b, c) => {
+  (t, a, b, c) => {
     expect(contains(b, a + b + c)).to.be.true;
   }
 );
